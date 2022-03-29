1a48e59f155c26fe8a6c7b82b08dc8e30209ba7a
Enric Vergara <donkikochan@gmail.com>
2022-03-29 17:12:14 +0200

    finished checking for email

--- Demo1/app/src/main/java/edu/uoc/demo1/MainActivity.java
+++ Demo1/app/src/main/java/edu/uoc/demo1/MainActivity.java
@@ -38,8 +38,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if (!hasFocus) {
-                    // code to execute when EditText loses focus
-                    Toast.makeText(getApplicationContext(), "Simple Button 1", Toast.LENGTH_LONG).show();//display the text of button1
+                    CheckUser();
+                }
+            }
+        });
+
+        pwd.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    CheckPwd();
                 }
             }
         });
@@ -47,47 +55,54 @@ public class MainActivity extends AppCompatActivity {
         checkCredentials.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String s_pwd = pwd.getText().toString();
-
-
-                char[] chars = s_pwd.toCharArray();
-                boolean containsDigit = false;
-                for(char c : chars){
-                    if(Character.isDigit(c)){
-                        containsDigit = true;
-                    }
+                if (CheckPwd() && CheckUser() ){
+                    //TODO.. connect to webservice or go to another view
                 }
-                //Check pwd:
-                if (s_pwd.length() <= 5){
-                    errorPwd.setVisibility(View.VISIBLE);
-                    errorPwd.setText("El password debe estar formado por más de 5 caracteres");
-                }else if (!containsDigit) {
-                    errorPwd.setVisibility(View.VISIBLE);
-                    errorPwd.setText("El password debe contener al menos un número");
-                } else{
-                     errorPwd.setVisibility(View.INVISIBLE);
-                }
-
-                //Check email:
-                String s_user = user.getText().toString();
-                String regex = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
-                        + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
-                Pattern p = Pattern.compile(regex);
-                Matcher m = p.matcher(s_user);
-                if (!m.matches()) {
-                    errorUser.setVisibility(View.VISIBLE);
-                    errorUser.setText("Debe ser un email con formato válido!");
-                } else{
-                    errorUser.setVisibility(View.INVISIBLE);
-                }
-
-
-                //checkCredentials.setVisibility(View.INVISIBLE);
             }
         });
 
+    }
+    private boolean CheckPwd(){
+        String s_pwd = pwd.getText().toString();
 
 
+        char[] chars = s_pwd.toCharArray();
+        boolean containsDigit = false;
+        for(char c : chars){
+            if(Character.isDigit(c)){
+                containsDigit = true;
+            }
+        }
+        //Check pwd:
+        boolean allIsCorrect = false;
+        if (s_pwd.length() <= 5){
+            errorPwd.setVisibility(View.VISIBLE);
+            errorPwd.setText("El password debe estar formado por más de 5 caracteres");
+        }else if (!containsDigit) {
+            errorPwd.setVisibility(View.VISIBLE);
+            errorPwd.setText("El password debe contener al menos un número");
+        } else{
+            errorPwd.setVisibility(View.INVISIBLE);
+            allIsCorrect = true;
+        }
 
+        return allIsCorrect;
+    }
+    private boolean CheckUser(){
+        boolean allIsCorrect = false;
+        //Check user email:
+        String s_user = user.getText().toString();
+        String regex = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+                + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(s_user);
+        if (!m.matches()) {
+            errorUser.setVisibility(View.VISIBLE);
+            errorUser.setText("Debe ser un email con formato válido!");
+        } else{
+            errorUser.setVisibility(View.INVISIBLE);
+            allIsCorrect = true;
+        }
+        return allIsCorrect;
     }
 }
