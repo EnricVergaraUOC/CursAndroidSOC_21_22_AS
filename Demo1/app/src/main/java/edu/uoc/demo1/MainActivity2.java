b251db6dff1c7d83848e670d77c77a5fbab402d7
Enric Vergara <donkikochan@gmail.com>
2022-03-30 17:19:42 +0200

    added input on board from the user

--- Demo1/app/src/main/java/edu/uoc/demo1/MainActivity2.java
+++ Demo1/app/src/main/java/edu/uoc/demo1/MainActivity2.java
@@ -36,6 +36,7 @@ public class MainActivity2 extends AppCompatActivity {
                 finish();
             }
         });
-
     }
+
+
 }
--- Demo1/app/src/main/java/edu/uoc/demo1/TresEnRaya.java
+++ Demo1/app/src/main/java/edu/uoc/demo1/TresEnRaya.java
@@ -3,11 +3,14 @@ package edu.uoc.demo1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class TresEnRaya extends AppCompatActivity {
+    private static final String EMPTY = "";
+    private Button[][] board = new Button[3][3];
+    private boolean turnOfX = false;
 
-    Button[][] myButton  = new Button[3][3];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,10 +20,39 @@ public class TresEnRaya extends AppCompatActivity {
             for (int j = 0; j < 3; j++){
                 String buttonID = "button"+i+j;
                 int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
-                myButton[i][j] = findViewById(resID);
+                board[i][j] = findViewById(resID);
+                board[i][j].setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Button btn = (Button) v;
+                        String value = btn.getText().toString();
+                        if (value.compareTo(EMPTY) == 0){
+                            if (turnOfX){
+                                btn.setText("X");
+                            }else{
+                                btn.setText("O");
+                            }
+
+                        }
+
+
+                        turnOfX = !turnOfX;
+                    }
+                });
             }
         }
 
+
+
+        ResetBoard();
+    }
+
+    private void ResetBoard(){
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                board[i][j].setText(EMPTY);
+            }
+        }
     }
 
 }
