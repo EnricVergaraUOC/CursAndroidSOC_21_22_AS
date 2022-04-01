71b474a5e81bad86cc3ec142211f2dd8cb0a00b5
Enric Vergara <donkikochan@gmail.com>
2022-04-01 18:29:18 +0200

    asking for permissions

--- Demo1/app/src/main/java/edu/uoc/demo1/Menu.java
+++ Demo1/app/src/main/java/edu/uoc/demo1/Menu.java
@@ -1,16 +1,24 @@
 package edu.uoc.demo1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Switch;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Menu extends AppCompatActivity {
+    public static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 101;
 
     public final static String AI_ENABLED = "AI_ENABLED";
 
@@ -42,5 +50,28 @@ public class Menu extends AppCompatActivity {
             }
         });
 
+    }
+
+    // function to check permission
+    public static boolean checkAndRequestPermissions(final Activity context) {
+        int WExtstorePermission = ContextCompat.checkSelfPermission(context,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int cameraPermission = ContextCompat.checkSelfPermission(context,
+                Manifest.permission.CAMERA);
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        if (cameraPermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.CAMERA);
+        }
+        if (WExtstorePermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded
+                    .add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(context, listPermissionsNeeded
+                            .toArray(new String[listPermissionsNeeded.size()]),
+                    REQUEST_ID_MULTIPLE_PERMISSIONS);
+            return false;
+        }
+        return true;
     }
 }
