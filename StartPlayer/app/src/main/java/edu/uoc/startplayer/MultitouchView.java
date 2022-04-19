d966d41d05f1fa1ec11f558ad67d5220514a96d3
Enric Vergara <donkikochan@gmail.com>
2022-04-19 18:15:31 +0200

    updated touch movement

--- StartPlayer/app/src/main/java/edu/uoc/startplayer/MultitouchView.java
+++ StartPlayer/app/src/main/java/edu/uoc/startplayer/MultitouchView.java
@@ -10,9 +10,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MultitouchView extends View implements View.OnTouchListener {
+
     private Paint paint;
-    private int x = 0;
-    private int y = 0;
 
     Map <Integer,Vector2D> map =  new HashMap<Integer,Vector2D>();
     public MultitouchView(Context context) {
@@ -21,7 +20,6 @@ public class MultitouchView extends View implements View.OnTouchListener {
         paint.setAntiAlias(true);
         paint.setARGB(255,255,0,0);
 
-        setFocusable(true);
         this.setOnTouchListener(this);
     }
 
@@ -29,33 +27,56 @@ public class MultitouchView extends View implements View.OnTouchListener {
     public void onDraw(Canvas canvas){
         super.onDraw(canvas);
 
-        canvas.drawCircle(x,y,100, paint);
+        for (Map.Entry<Integer, Vector2D> pair: map.entrySet()) {
+            int x = pair.getValue().GetPosX();
+            int y = pair.getValue().GetPosY();
+            canvas.drawCircle(x,y,100, paint);
+        }
+
 
 
     }
 
     @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
+    public boolean onTouch(View view, MotionEvent event){
 
-        int pointerIndex = motionEvent.getActionIndex();
-        int pointerID = motionEvent.getPointerId(pointerIndex);
-        int maskedAction = motionEvent.getActionMasked();
+        int pointerIndex = event.getActionIndex();
+        int pointerID = event.getPointerId(pointerIndex);
+        int maskedAction = event.getActionMasked();
 
         switch (maskedAction){
-            case MotionEvent.ACTION_DOWN:
-                //TODO: registrar el touch con el id y pos
-                motionEvent.getX(pointerIndex);
+            case MotionEvent.ACTION_POINTER_DOWN:
+            case MotionEvent.ACTION_DOWN: {
+                int x = (int) event.getX(pointerIndex);
+                int y = (int) event.getY(pointerIndex);
+                Vector2D newPoint = new Vector2D(x, y);
+                map.put(pointerID, newPoint);
+            }
+
                 break;
-            case MotionEvent.ACTION_MOVE:
-                //TODO: actualizar la pos del touch con el id
+            case MotionEvent.ACTION_MOVE:{
+                int size = event.getPointerCount();
+                for (int i = 0; i < size; i++) {
+                    Vector2D point = map.get(event.getPointerId(i));
+                    if (point != null) {
+                        point.SetPosX((int)event.getX(i));
+                        point.SetPosY((int)event.getY(i));
+                    }
+                }
+
+                int x = (int)event.getX(pointerIndex);
+                int y = (int)event.getY(pointerIndex);
+                map.get(pointerID).SetPosX(x);
+                map.get(pointerID).SetPosY(y);
+            }
+
                 break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_POINTER_UP:
             case MotionEvent.ACTION_CANCEL:
-                //TODO: eliminar el touch cno el id.
+                map.remove(pointerID);
                 break;
         }
-
-
-
 
 
         invalidate();
