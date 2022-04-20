6320540d20b2b1644d8a290f4203a0ff93b06b9f
Enric Vergara <donkikochan@gmail.com>
2022-04-20 15:39:54 +0200

    manage of multitouch finished

--- StartPlayer/app/src/main/java/edu/uoc/startplayer/MultitouchView.java
+++ StartPlayer/app/src/main/java/edu/uoc/startplayer/MultitouchView.java
@@ -2,18 +2,31 @@ package edu.uoc.startplayer;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class MultitouchView extends View implements View.OnTouchListener {
 
     private Paint paint;
+    private static final int SIZE = 200;
+    CountDownTimer cTimer = null;
+    private int[] colors = { Color.BLUE, Color.GREEN, Color.MAGENTA,
+            Color.BLACK, Color.CYAN, Color.GRAY, Color.RED, Color.DKGRAY,
+            Color.LTGRAY, Color.YELLOW };
+    private int colorIndex = 0;
 
-    Map <Integer,Vector2D> map =  new HashMap<Integer,Vector2D>();
+    ArrayList<Vector2D> map = new ArrayList<Vector2D>();
+    private int playerSelected = -1;
+
     public MultitouchView(Context context) {
         super(context);
         paint = new Paint();
@@ -27,11 +40,30 @@ public class MultitouchView extends View implements View.OnTouchListener {
     public void onDraw(Canvas canvas){
         super.onDraw(canvas);
 
-        for (Map.Entry<Integer, Vector2D> pair: map.entrySet()) {
-            int x = pair.getValue().GetPosX();
-            int y = pair.getValue().GetPosY();
-            canvas.drawCircle(x,y,100, paint);
+
+        if (playerSelected == -1){
+            for (int i = 0; i < map.size(); i++){
+                int x = map.get(i).posX;
+                int y = map.get(i).posY;
+                int c = map.get(i).color;
+                paint.setColor(c);
+                canvas.drawCircle(x,y,SIZE, paint);
+            }
+        }else{
+
+            for (int i = 0; i < map.size(); i++){
+                int x = map.get(i).posX;
+                int y = map.get(i).posY;
+                if(i == playerSelected){
+                    paint.setColor(Color.RED);
+                }else{
+                    paint.setColor(Color.BLACK);
+                }
+                canvas.drawCircle(x,y,SIZE, paint);
+            }
+
         }
+
 
 
 
@@ -39,42 +71,75 @@ public class MultitouchView extends View implements View.OnTouchListener {
 
     @Override
     public boolean onTouch(View view, MotionEvent event){
-
+        if (playerSelected!= -1){
+            return true;
+        }
         int pointerIndex = event.getActionIndex();
         int pointerID = event.getPointerId(pointerIndex);
         int maskedAction = event.getActionMasked();
-
+        Log.i("touch_enric", ""+ pointerID);
         switch (maskedAction){
             case MotionEvent.ACTION_POINTER_DOWN:
             case MotionEvent.ACTION_DOWN: {
                 int x = (int) event.getX(pointerIndex);
                 int y = (int) event.getY(pointerIndex);
-                Vector2D newPoint = new Vector2D(x, y);
-                map.put(pointerID, newPoint);
+                Vector2D newPoint = new Vector2D(x, y, pointerID, colors[colorIndex% colors.length]);
+                colorIndex++;
+                map.add(newPoint);
+
+                if (cTimer != null){
+                    cTimer.cancel();
+                }
+                cTimer = new CountDownTimer(3000, 1000)
+                {
+                    public void onTick(long millisUntilFinished) { }
+                    public void onFinish() {
+
+                        int min = 0;
+                        int max = map.size();
+                        Random random = new Random();
+                        playerSelected = random.nextInt(max + min) + min;
+
+                        invalidate();
+                        cTimer.cancel();
+
+                    }
+                };
+                cTimer.start();
             }
 
                 break;
             case MotionEvent.ACTION_MOVE:{
                 int size = event.getPointerCount();
                 for (int i = 0; i < size; i++) {
-                    Vector2D point = map.get(event.getPointerId(i));
-                    if (point != null) {
-                        point.SetPosX((int)event.getX(i));
-                        point.SetPosY((int)event.getY(i));
+
+                    boolean found = false;
+                    int index = 0;
+                    while(!found && index < map.size()) {
+                        if (map.get(index).ID == event.getPointerId(i)) {
+                            map.get(index).posX = (int) event.getX(i);
+                            map.get(index).posY = (int) event.getY(i);
+                            found = true;
+                        }
+                        index++;
                     }
                 }
 
-                int x = (int)event.getX(pointerIndex);
-                int y = (int)event.getY(pointerIndex);
-                map.get(pointerID).SetPosX(x);
-                map.get(pointerID).SetPosY(y);
             }
 
                 break;
             case MotionEvent.ACTION_UP:
             case MotionEvent.ACTION_POINTER_UP:
             case MotionEvent.ACTION_CANCEL:
-                map.remove(pointerID);
+                boolean found = false;
+                int index = 0;
+                while(!found && index < map.size()){
+                    if (map.get(index).ID == pointerID){
+                        map.remove(index);
+                        found = true;
+                    }
+                    index++;
+                }
                 break;
         }
 
--- StartPlayer/app/src/main/java/edu/uoc/startplayer/Vector2D.java
+++ StartPlayer/app/src/main/java/edu/uoc/startplayer/Vector2D.java
@@ -1,20 +1,20 @@
 package edu.uoc.startplayer;
 
+import android.graphics.Color;
+
 public class Vector2D {
-    private int posX;
-    private int posY;
-    Vector2D(int posX, int posY){
+    int posX;
+    int posY;
+    int ID;
+    int color;
+    Vector2D(int posX, int posY, int ID, int color){
         this.posX = posX;
         this.posY = posY;
+        this.ID = ID;
+        this.color = color;
     }
     Vector2D(){
         this.posX = 0;
         this.posY = 0;
     }
-
-    int GetPosX(){ return posX;}
-    int GetPosY(){ return posY;}
-    void SetPosX(int x){ posX = x;}
-    void SetPosY(int y){ posY = y;}
-
 }
