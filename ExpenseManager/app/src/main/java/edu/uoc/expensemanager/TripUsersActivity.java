eece89fb0b20de1eb48b77c4713c84dd6fba7400
Enric Vergara <donkikochan@gmail.com>
2022-04-26 18:02:30 +0200

    creating new projecte: ExpenseManager

---
+++ ExpenseManager/app/src/main/java/edu/uoc/expensemanager/TripUsersActivity.java
@@ -0,0 +1,14 @@
+package edu.uoc.expensemanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TripUsersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_users);
+    }
+}
