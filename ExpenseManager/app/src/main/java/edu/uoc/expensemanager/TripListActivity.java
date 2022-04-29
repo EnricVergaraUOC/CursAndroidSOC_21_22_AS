c840d9c7f186803cddaae24dcb979b95c5e52f4a
Enric Vergara <donkikochan@gmail.com>
2022-04-29 17:36:33 +0200

    Added layout  of profile and edittrip

--- ExpenseManager/app/src/main/java/edu/uoc/expensemanager/TripListActivity.java
+++ ExpenseManager/app/src/main/java/edu/uoc/expensemanager/TripListActivity.java
@@ -2,13 +2,38 @@ package edu.uoc.expensemanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class TripListActivity extends AppCompatActivity {
-
+    FloatingActionButton btnAddNewTrip;
+    ImageButton btnViewProfile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip_list);
+        btnAddNewTrip = findViewById(R.id.btn_add_new_trip);
+        btnViewProfile = findViewById(R.id.btn_vew_profile);
+
+        //Add actions to the buttons:
+        btnAddNewTrip.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent k = new Intent(TripListActivity.this, TripEditActivity.class);
+                startActivity(k);
+            }
+        });
+
+        btnViewProfile.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent k = new Intent(TripListActivity.this, UserProfileActivity.class);
+                startActivity(k);
+            }
+        });
+
     }
 }
--- ExpenseManager/app/src/main/java/edu/uoc/expensemanager/UserProfileActivity.java
+++ ExpenseManager/app/src/main/java/edu/uoc/expensemanager/UserProfileActivity.java
@@ -2,13 +2,43 @@ package edu.uoc.expensemanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class UserProfileActivity extends AppCompatActivity {
 
+    Button btnLogout;
+    Button btnSave;
+    TextView txtUserName;
+    ImageView imgAvatar;
+    EditText inputName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+        btnLogout = findViewById(R.id.btn_logout);
+        btnSave = findViewById(R.id.btn_save);
+        txtUserName = findViewById(R.id.txt_username);
+        imgAvatar = findViewById(R.id.img_avatar);
+        inputName  = findViewById(R.id.input_name);
+
+        //Add actions to the buttons:
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //TODO...
+            }
+        });
+
     }
+
 }
