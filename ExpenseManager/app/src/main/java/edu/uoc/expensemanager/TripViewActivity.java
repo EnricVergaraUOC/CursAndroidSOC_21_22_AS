5db18ec8b16a0662576db37ca00bf63f92603498
Enric Vergara <donkikochan@gmail.com>
2022-04-29 19:13:29 +0200

    finished the layout of the other activities

--- ExpenseManager/app/src/main/java/edu/uoc/expensemanager/TripViewActivity.java
+++ ExpenseManager/app/src/main/java/edu/uoc/expensemanager/TripViewActivity.java
@@ -2,13 +2,52 @@ package edu.uoc.expensemanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class TripViewActivity extends AppCompatActivity {
+
+    ImageButton btnEditTrip;
+    FloatingActionButton btnAddNewExpense;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip_view);
+
+        btnEditTrip = findViewById(R.id.btn_edit_trip);
+        btnAddNewExpense = findViewById(R.id.btn_add_new_expense);
+
+        //Add actions to the buttons:
+        btnEditTrip.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent k = new Intent(TripViewActivity.this, TripEditActivity.class);
+                startActivity(k);
+            }
+        });
+        btnAddNewExpense.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent k = new Intent(TripViewActivity.this, ExpenseActivity.class);
+                startActivity(k);
+            }
+        });
+
+
+        //------Temporal code for debugging purposes:
+        Button btnAuxViewExpense = findViewById(R.id.btn_aux_view_expense);
+        btnAuxViewExpense.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent k = new Intent(TripViewActivity.this, ExpenseActivity.class);
+                startActivity(k);
+            }
+        });
+
+
     }
 }
