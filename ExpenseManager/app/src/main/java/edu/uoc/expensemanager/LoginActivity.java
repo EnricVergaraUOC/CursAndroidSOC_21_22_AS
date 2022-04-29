88b2b3feab3beb2a23d2060e733657f9ec218e38
Enric Vergara <donkikochan@gmail.com>
2022-04-29 15:35:14 +0200

    adding activities: Login, Register and TripList

--- ExpenseManager/app/src/main/java/edu/uoc/expensemanager/LoginActivity.java
+++ ExpenseManager/app/src/main/java/edu/uoc/expensemanager/LoginActivity.java
@@ -2,13 +2,41 @@ package edu.uoc.expensemanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    Button btnRegister;
+    TextView inputUserName;
+    TextView inputPwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+        btnLogin = findViewById(R.id.btn_login);
+        btnRegister = findViewById(R.id.btn_goto_register);
+        inputPwd = findViewById(R.id.input_pwd);
+        inputUserName = findViewById(R.id.input_username);
+
+        //Add actions to the buttons:
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent k = new Intent(LoginActivity.this, TripListActivity.class);
+                startActivity(k);
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent k = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(k);
+            }
+        });
     }
 }
--- ExpenseManager/app/src/main/java/edu/uoc/expensemanager/RegisterActivity.java
+++ ExpenseManager/app/src/main/java/edu/uoc/expensemanager/RegisterActivity.java
@@ -2,13 +2,32 @@ package edu.uoc.expensemanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    Button btnRegister;
+    TextView inputUserName;
+    TextView inputPwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        btnRegister = findViewById(R.id.btn_register);
+        inputPwd = findViewById(R.id.input_pwd);
+        inputUserName = findViewById(R.id.input_username);
+
+        //Add actions to the buttons:
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
