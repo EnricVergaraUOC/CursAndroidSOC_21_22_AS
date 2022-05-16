4ab15ac92a3a548ccd084cda638fae44cc07c442
Enric Vergara <donkikochan@gmail.com>
2022-05-16 16:31:41 +0200

    updated funcitonality of register

--- ExpenseManager/app/src/main/java/edu/uoc/expensemanager/ui/RegisterActivity.java
+++ ExpenseManager/app/src/main/java/edu/uoc/expensemanager/ui/RegisterActivity.java
@@ -1,12 +1,22 @@
 package edu.uoc.expensemanager.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import edu.uoc.expensemanager.R;
 
@@ -15,7 +25,11 @@ public class RegisterActivity extends AppCompatActivity {
     Button btnRegister;
     TextView inputUserName;
     TextView inputPwd;
+    private FirebaseAuth mAuth;
 
+    public void kk (){
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +42,27 @@ public class RegisterActivity extends AppCompatActivity {
         //Add actions to the buttons:
         btnRegister.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                mAuth = FirebaseAuth.getInstance();
+
+
+                mAuth.createUserWithEmailAndPassword(inputUserName.getText().toString(), inputPwd.getText().toString())
+                        .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (!task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    FirebaseUser user = mAuth.getCurrentUser();
+
+                                    Toast.makeText(RegisterActivity.this,"Successful Registration", Toast.LENGTH_LONG).show();
+                                    finish();
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    //Log.w("Register", "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(RegisterActivity.this,"Error with Register: "+ task.getException(), Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
                 finish();
             }
         });
