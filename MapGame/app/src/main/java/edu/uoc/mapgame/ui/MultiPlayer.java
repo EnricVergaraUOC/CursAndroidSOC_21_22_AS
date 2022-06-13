f139586e7cd5e92ab0ff068fd2c56f8b93acfb1a
Enric Vergara <donkikochan@gmail.com>
2022-06-13 17:11:46 +0200

    ...

--- MapGame/app/src/main/java/edu/uoc/mapgame/ui/MultiPlayer.java
+++ MapGame/app/src/main/java/edu/uoc/mapgame/ui/MultiPlayer.java
@@ -1,18 +1,120 @@
 package edu.uoc.mapgame.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import edu.uoc.mapgame.R;
 
 public class MultiPlayer extends AppCompatActivity {
+
+    EditText lblServerName;
+    Button createServer;
+    Button joinToServer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multi_player);
 
+        createServer = findViewById(R.id.btnCreateServer);
+        joinToServer = findViewById(R.id.btnJoinToServer);
+        lblServerName = findViewById(R.id.txt_sererName);
+
+        createServer.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                boolean allOk = CheckServerName();
+                if (allOk){
+                    CreateServer();
+                }
+            }
+        });
+
+        joinToServer.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                boolean allOk = CheckServerName();
+                if (allOk){
+
+                }
+            }
+        });
         this.getSupportActionBar().setTitle("MultiPlayer");
+    }
+
+    boolean CheckServerName (){
+        boolean allIsOk;
+        if (lblServerName.getText().toString().compareTo("") != 0){
+            allIsOk = true;
+        }else{
+            allIsOk = false;
+            new AlertDialog.Builder(MultiPlayer.this)
+                    .setTitle("Server name can't be empty")
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            //Nothing to do.
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
+
+        return allIsOk;
+    }
+
+    void CreateServer(){
+
+        Map<String, Object> newParty = new HashMap<>();
+        newParty.put("id", lblServerName.getText().toString());
+        newParty.put("readyToNextQuestion", false);
+        newParty.put("currentQuestion", 0);
+        Map<String, Object> newPlayer = new HashMap<>();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        newPlayer.put("email", currentUser.getEmail());
+        newPlayer.put("score", 0);
+
+        List<Object> players = new ArrayList<Object>();
+        players.add(newPlayer);
+        newParty.put("players", players);
+
+
+
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        // Add a new document with a generated ID
+        db.collection("Multiplayer")
+                .add(newParty)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        //Go to WaitingRoom
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        String error = e.toString();
+                        //ShowErrorMessage(error);
+                    }
+                });
     }
 }
---
+++ MapGame/app/src/main/java/edu/uoc/mapgame/ui/WaitingRoom.java
@@ -0,0 +1,14 @@
+package edu.uoc.mapgame.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import edu.uoc.mapgame.R;
+public class WaitingRoom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_waiting_room);
+    }
+}
